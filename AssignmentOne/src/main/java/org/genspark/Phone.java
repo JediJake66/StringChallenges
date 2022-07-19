99398abfbe28ec06bff6a9cc04b9f8c70558ecb4
Jacob A Stout <92182042+JediJake66@users.noreply.github.com>
2022-07-19 19:03:40 -0400

    XML Config

---
+++ AssignmentOne/src/main/java/org/genspark/Phone.java
@@ -0,0 +1,23 @@
+package org.genspark;
+
+public class Phone {
+    private String mob;
+
+
+    public Phone(String mob) {
+        this.mob = mob;
+    }
+
+    @Override
+    public String toString() {
+        return mob;
+    }
+
+    public String getMob() {
+        return mob;
+    }
+
+    public void setMob(String mob) {
+        this.mob = mob;
+    }
+}
---
+++ AssignmentOne/src/main/java/org/genspark/Student.java
@@ -0,0 +1,68 @@
+package org.genspark;
+
+import java.util.List;
+
+public class Student {
+
+    private int id;
+    private String name;
+    private List<Phone> ph;
+    private Address add;
+
+    public Student(int id, String name, List<Phone> ph, Address add) {
+        this.id = id;
+        this.name = name;
+        this.ph = ph;
+        this.add = add;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "ID: " + id +
+                ", Name: '" + name + '\'' +
+                ", Phone Number: " + ph +
+                ", " + add +
+                '}';
+    }
+
+    public  void findStudent()
+    {
+        System.out.println("Student Found --> " + toString());
+    }
+
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Phone> getPh() {
+        return ph;
+    }
+
+    public Address getAdd() {
+        return add;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPh(List<Phone> ph) {
+        this.ph = ph;
+    }
+
+    public void setAdd(Address add) {
+        this.add = add;
+    }
+}
