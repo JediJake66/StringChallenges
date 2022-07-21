a77d926cc11215a29a253aef66383aacdcd250df
Jacob A Stout <92182042+JediJake66@users.noreply.github.com>
2022-07-21 13:14:42 -0400

    Java Configuration

---
+++ AssignmentThree/src/main/java/org/genspark/Student.java
@@ -0,0 +1,31 @@
+package org.genspark;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class Student
+{
+    private int id=66;
+    private String name="Jacob Stout";
+    @Autowired
+    private List<Phone> ph;
+    @Autowired
+    private Address add;
+
+    @Override
+    public String toString() {
+        return '\n' +"Student ID: " + id + '\n'+
+                "Name: " + name + '\n' +
+                "Phone Number: " + ph +
+                add ;
+    }
+
+    public  void findStudent()
+    {
+        System.out.println("Student Found --> " + toString());
+    }
+
+}
